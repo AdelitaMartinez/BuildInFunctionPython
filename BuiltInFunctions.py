68847eb8fb471cb494d373a39ad47c9d7954e892
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 13:07:13 -0600

    Added notes for min

--- BuiltInFunctions.py
+++ BuiltInFunctions.py
@@ -5,5 +5,13 @@
 
 # Get a number from user and display absolute 
 num = float(input('Please enter a number: '))
+# abs() returns the absolute value of a number
 absolute = abs(num)
 print('The absolute value is',absolute)
+
+# Find the minimum of two numbers
+num1 = float(input('Please enter first number: '))
+num2 = float(input('Please enter second number: '))
+# min() returns the largest item
+minimum_value = min(num1,num2)
+print('The lowest of those two numbers is', minimum_value)
