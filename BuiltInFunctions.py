ad0247ffe9bb12147c051de82b57eff16deb14d3
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 13:18:43 -0600

    Adding notes for more functions

--- BuiltInFunctions.py
+++ BuiltInFunctions.py
@@ -3,15 +3,27 @@
 # Date: 16 May 2024
 # Purpose: Demonstrate built in python functions
 
-# Get a number from user and display absolute 
-num = float(input('Please enter a number: '))
-# abs() returns the absolute value of a number
-absolute = abs(num)
-print('The absolute value is',absolute)
+# # Get a number from user and display absolute 
+# num = float(input('Please enter a number: '))
+# # abs() returns the absolute value of a number
+# absolute = abs(num)
+# print('The absolute value is',absolute)
 
-# Find the minimum of two numbers
-num1 = float(input('Please enter first number: '))
-num2 = float(input('Please enter second number: '))
-# min() returns the largest item
-minimum_value = min(num1,num2)
-print('The lowest of those two numbers is', minimum_value)
+# # Find the minimum of two numbers
+# num1 = float(input('Please enter first number: '))
+# num2 = float(input('Please enter second number: '))
+# # min() returns the largest item
+# minimum_value = min(num1,num2)
+# print('The lowest of those two numbers is', minimum_value)
+
+# #Round a number
+# new_num = float(input('Please enter a decimal number: '))
+# # round() is used to round numbers
+# rounded = round(new_num)
+# print('The rounded value is', rounded)
+
+# Raise a number to a power 
+decimal_number = float(input('Please enter a decimal number: '))
+exponenet = float(input("Please enter a value to raise number to: "))
+raised_value = pow(decimal_number, exponenet)
+print('The value is', raised_value)
