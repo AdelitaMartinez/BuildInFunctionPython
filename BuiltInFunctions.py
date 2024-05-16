719bbb4b77cad00b6f4d6525f92f02e09305a339
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 12:59:56 -0600

    Practicing floats

--- BuiltInFunctions.py
+++ BuiltInFunctions.py
@@ -2,3 +2,8 @@
 # Programmer: Adelita Martinez 
 # Date: 16 May 2024
 # Purpose: Demonstrate built in python functions
+
+# Get a number from user and display absolute 
+num = float(input('Please enter a number: '))
+absolute = abs(num)
+print('The absolute value is',absolute)
